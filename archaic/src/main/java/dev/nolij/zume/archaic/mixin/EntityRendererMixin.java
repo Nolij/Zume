4efc6f38a709e990f9e13b9635fbed4d183464fe
Nolij <xdmatthewbx@mixhosting.xyz>
2023-12-15 17:30:57 -0500

    make archaic forge mixins vintage-compatible

--- archaic/src/main/java/dev/nolij/zume/archaic/mixin/EntityRendererMixin.java
+++ archaic/src/main/java/dev/nolij/zume/archaic/mixin/EntityRendererMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.nolij.zume.common.Zume;
 import dev.nolij.zume.archaic.ArchaicZume;
 import net.minecraft.client.renderer.EntityRenderer;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,12 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
 
-	@Inject(method = "updateCameraAndRender", at = @At("HEAD"))
+	@Dynamic
+	@Inject(method = {
+		"updateCameraAndRender", // archaic
+		"func_181560_a(FJ)V" // vintage
+	}, at = @At("HEAD"))
 	public void zume$render$HEAD(CallbackInfo ci) {
 		Zume.render();
 	}
-
-
+	
 	@Inject(method = "getFOVModifier", at = @At("TAIL"), cancellable = true)
 	public void zume$getFOV$TAIL(CallbackInfoReturnable<Float> cir) {
 		Zume.realFOV = cir.getReturnValueF();
@@ -27,7 +31,11 @@ public class EntityRendererMixin {
 		}
 	}
 
-	@ModifyExpressionValue(method = {"updateCameraAndRender", "updateRenderer"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;smoothCamera:Z"))
+	@Dynamic
+	@ModifyExpressionValue(method = {
+		"updateCameraAndRender", "updateRenderer", // archaic
+		"func_181560_a(FJ)V", "func_78464_a()V" // vintage
+	}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;smoothCamera:Z"))
 	public boolean zume$updateMouse$smoothCameraEnabled(boolean original) {
 		if (Zume.CONFIG.enableCinematicZoom && ArchaicZume.INSTANCE.isZoomPressed()) {
 			return true;
@@ -36,7 +44,11 @@ public class EntityRendererMixin {
 		return original;
 	}
 
-	@ModifyExpressionValue(method = {"updateCameraAndRender", "updateRenderer"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;mouseSensitivity:F"))
+	@Dynamic
+	@ModifyExpressionValue(method = {
+		"updateCameraAndRender", "updateRenderer", // archaic
+		"func_181560_a(FJ)V", "func_78464_a()V" // vintage
+	}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;mouseSensitivity:F"))
 	public float zume$updateMouse$mouseSensitivity(float original) {
 		return (float) Zume.getMouseSensitivity(original);
 	}
--- archaic/src/main/java/dev/nolij/zume/archaic/mixin/MinecraftMixin.java
+++ archaic/src/main/java/dev/nolij/zume/archaic/mixin/MinecraftMixin.java
@@ -5,13 +5,18 @@ import dev.nolij.zume.common.Zume;
 import dev.nolij.zume.archaic.ArchaicZume;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.InventoryPlayer;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
-	@WrapWithCondition(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/InventoryPlayer;changeCurrentItem(I)V"))
+	@Dynamic
+	@WrapWithCondition(method = {
+		"runTick", // archaic 
+		"func_184124_aB()V" // vintage
+	}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/InventoryPlayer;changeCurrentItem(I)V"))
 	public boolean onMouseScroll$scrollInHotbar(InventoryPlayer instance, int scrollAmount) {
 		if (Zume.CONFIG.enableZoomScrolling)
 			Zume.scrollDelta += scrollAmount;
