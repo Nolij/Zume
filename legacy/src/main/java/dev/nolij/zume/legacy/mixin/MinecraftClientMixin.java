a47204c05ec44aaa47700d0cd9f90bd3a48af7f3
Nolij <xdmatthewbx@mixhosting.xyz>
2023-12-15 17:30:33 -0500

    cleanup

--- legacy/src/main/java/dev/nolij/zume/legacy/mixin/MinecraftClientMixin.java
+++ legacy/src/main/java/dev/nolij/zume/legacy/mixin/MinecraftClientMixin.java
@@ -5,13 +5,18 @@ import dev.nolij.zume.common.Zume;
 import dev.nolij.zume.legacy.LegacyZume;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 	
-	@WrapWithCondition(method = {"tick", "method_12141()V"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;scrollInHotbar(I)V"))
+	@Dynamic
+	@WrapWithCondition(method = {
+		"tick", // archaic 
+		"method_12141()V" // vintage
+	}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;scrollInHotbar(I)V"))
 	public boolean onMouseScroll$scrollInHotbar(PlayerInventory instance, int scrollAmount) {
 		if (Zume.CONFIG.enableZoomScrolling)
 			Zume.scrollDelta += scrollAmount > 0 ? 1 : -1;
