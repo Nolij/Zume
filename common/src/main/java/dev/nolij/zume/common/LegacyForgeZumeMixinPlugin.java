5f8f86052db61ee15cc77df4e985eec65b69a258
Nolij <xdmatthewbx@mixhosting.xyz>
2023-12-21 18:30:41 -0500

    make this more readable

--- common/src/main/java/dev/nolij/zume/common/LegacyForgeZumeMixinPlugin.java
+++ common/src/main/java/dev/nolij/zume/common/LegacyForgeZumeMixinPlugin.java
@@ -4,11 +4,16 @@ public class LegacyForgeZumeMixinPlugin extends ZumeMixinPlugin {
 	
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-		return (
-			Zume.ZUME_VARIANT == ZumeVariant.ARCHAIC_FORGE &&
-			mixinClassName.startsWith("dev.nolij.zume.legacyforge.mixin.archaic")) || (
-			Zume.ZUME_VARIANT == ZumeVariant.VINTAGE_FORGE &&
-			mixinClassName.startsWith("dev.nolij.zume.legacyforge.mixin.vintage"));
+		switch (Zume.ZUME_VARIANT) {
+			case ARCHAIC_FORGE -> {
+				return mixinClassName.startsWith("dev.nolij.zume.legacyforge.mixin.archaic");
+			}
+			case VINTAGE_FORGE -> {
+				return mixinClassName.startsWith("dev.nolij.zume.legacyforge.mixin.vintage");
+			}
+		}
+		
+		return false;
 	}
 	
 }
