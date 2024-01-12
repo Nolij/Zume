65bd236405c87582eba7e0c11f0951f1d33ae306
Nolij <xdmatthewbx@mixhosting.xyz>
2024-01-11 20:54:29 -0500

    cleanup

--- lexforge/src/main/java/dev/nolij/zume/lexforge/LexZume.java
+++ lexforge/src/main/java/dev/nolij/zume/lexforge/LexZume.java
@@ -8,7 +8,6 @@ import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
