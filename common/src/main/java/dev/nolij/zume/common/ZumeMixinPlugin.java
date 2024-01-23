55c8c7ba6a1bae2c2d983ef427137c3191e48781
Nolij <xdmatthewbx@mixhosting.xyz>
2024-01-22 22:49:22 -0500

    c o n d e n s e

--- common/src/main/java/dev/nolij/zume/common/ZumeMixinPlugin.java
+++ common/src/main/java/dev/nolij/zume/common/ZumeMixinPlugin.java
@@ -7,11 +7,23 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
-public abstract class ZumeMixinPlugin implements IMixinConfigPlugin {
+public class ZumeMixinPlugin implements IMixinConfigPlugin {
+	
+	private String implementationMixinPackage = null;
 	
 	@Override
 	public void onLoad(String mixinPackage) {
 		Zume.calculateZumeVariant();
+        if (Zume.ZUME_VARIANT != null)
+            implementationMixinPackage = "dev.nolij.zume." + Zume.ZUME_VARIANT.name + ".mixin.";
+	}
+	
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if (implementationMixinPackage == null)
+			return false;
+		
+		return mixinClassName.startsWith(implementationMixinPackage);
 	}
 	
 	@Override
@@ -20,9 +32,7 @@ public abstract class ZumeMixinPlugin implements IMixinConfigPlugin {
 	}
 	
 	@Override
-	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-		
-	}
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
 	
 	@Override
 	public List<String> getMixins() {
@@ -30,13 +40,9 @@ public abstract class ZumeMixinPlugin implements IMixinConfigPlugin {
 	}
 	
 	@Override
-	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-		
-	}
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 	
 	@Override
-	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-		
-	}
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 	
 }
--- common/src/main/java/dev/nolij/zume/common/ZumeVariant.java
+++ common/src/main/java/dev/nolij/zume/common/ZumeVariant.java
@@ -2,14 +2,22 @@ package dev.nolij.zume.common;
 
 public enum ZumeVariant {
 	
-	MODERN,
-	PRIMITIVE,
-	LEGACY,
-	ARCHAIC_FORGE,
-	VINTAGE_FORGE,
-	NEOFORGE,
-	LEXFORGE,
-	LEXFORGE18,
-	LEXFORGE16,
+	MODERN("modern"),
+	PRIMITIVE("primitive"),
+	LEGACY("legacy"),
+	ARCHAIC_FORGE("archaic"),
+	VINTAGE_FORGE("vintage"),
+	NEOFORGE("neoforge"),
+	LEXFORGE("lexforge"),
+	LEXFORGE18("lexforge18"),
+	LEXFORGE16("lexforge16"),
+	
+	;
+	
+	public final String name;
+	
+	ZumeVariant(String name) {
+		this.name = name;
+	}
 	
 }
