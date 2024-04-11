edebeff550b3bc789e117656f2868159860472a4
Nolij <xdmatthewbx@mixhosting.xyz>
2024-04-11 17:02:58 -0400

    replace lexforge18 mixin with more method handle hacks

--- lexforge18/src/main/java/dev/nolij/zume/lexforge18/LexZume18.java
+++ lexforge18/src/main/java/dev/nolij/zume/lexforge18/LexZume18.java
@@ -3,8 +3,10 @@ package dev.nolij.zume.lexforge18;
 import dev.nolij.zume.common.CameraPerspective;
 import dev.nolij.zume.common.IZumeImplementation;
 import dev.nolij.zume.common.Zume;
+import dev.nolij.zume.common.util.MethodHandleHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
@@ -13,8 +15,27 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLPaths;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
 @Mod(Zume.MOD_ID)
 public class LexZume18 implements IZumeImplementation {
+	
+	private static final Class<?> FOV_EVENT_CLASS = MethodHandleHelper.firstNonNull(
+		MethodHandleHelper.getClassOrNull("net.minecraftforge.client.event.EntityViewRenderEvent$FieldOfView"),
+		MethodHandleHelper.getClassOrNull("net.minecraftforge.client.event.EntityViewRenderEvent$FOVModifier")
+	);
+	private static final MethodHandle GET_FOV = MethodHandleHelper.getMethodOrNull(
+		FOV_EVENT_CLASS,
+		"getFOV",
+		MethodType.methodType(double.class, EntityViewRenderEvent.class)
+	);
+	private static final MethodHandle SET_FOV = MethodHandleHelper.getMethodOrNull(
+		FOV_EVENT_CLASS,
+		"setFOV",
+		MethodType.methodType(void.class, EntityViewRenderEvent.class, double.class),
+		double.class
+	);
 	
 	public LexZume18() {
 		if (!FMLEnvironment.dist.isClient())
@@ -33,6 +54,7 @@ public class LexZume18 implements IZumeImplementation {
 		}
 		
 		MinecraftForge.EVENT_BUS.addListener(this::render);
+		MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::calculateFOV);
 		MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGHEST, this::onMouseScroll);
 	}
 	
@@ -59,6 +81,16 @@ public class LexZume18 implements IZumeImplementation {
 	private void render(TickEvent.RenderTickEvent event) {
 		if (event.phase == TickEvent.Phase.START) {
 			Zume.render();
+		}
+	}
+	
+	private void calculateFOV(EntityViewRenderEvent event) {
+		if (event.getClass() == FOV_EVENT_CLASS && Zume.shouldHookFOV()) {
+			try {
+				SET_FOV.invokeExact(event, Zume.transformFOV((double) GET_FOV.invokeExact(event)));
+			} catch (Throwable e) {
+				throw new AssertionError(e);
+			}
 		}
 	}
 	
