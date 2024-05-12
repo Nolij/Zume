832227cc1a9a34cf769de068836c17c4389dd3ad
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-05-12 14:01:28 -0400

    remove switch java 21 moment

--- zson/src/main/java/dev/nolij/zson/Zson.java
+++ zson/src/main/java/dev/nolij/zson/Zson.java
@@ -35,20 +35,24 @@ public final class Zson {
 	}
 	
 	private String value(Object value) {
-		return switch (value) {
-			case ZsonMap zson -> {
-				try {
-					yield stringify(zson).replace("\n", "\n" + indent);
-				} catch (StackOverflowError e) {
-					throw new StackOverflowError("Map is circular");
-				}
+		if(value instanceof ZsonMap zson) {
+			try {
+				return stringify(zson).replace("\n", "\n" + indent);
+			} catch (StackOverflowError e) {
+				throw new StackOverflowError("Map is circular");
 			}
-			case String s -> "\"" + s.replace("\"", "\\\"") + "\"";
-			case Number n -> n.toString();
-			case Boolean b -> b.toString();
-			case null -> "null";
-			default -> throw new IllegalArgumentException("Unsupported value type: " + value.getClass().getName());
-		};
+		} else if(value instanceof String s) {
+			return '"' + s
+				.replace("\"", "\\\"")
+				.replace("\n", "\\n")
+				.replace("\r", "\\r")
+				.replace("\t", "\\t")
+				+ '"';
+		} else if(value instanceof Number || value instanceof Boolean || value == null) {
+			return String.valueOf(value);
+		} else {
+			throw new IllegalArgumentException("Unsupported value type: " + value.getClass().getName());
+		}
 	}
 	
 	public static Map.Entry<Map.Entry<String, String>, Object> entry(String key, String comment, Object value) {
