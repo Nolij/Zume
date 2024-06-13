d1bd8ced3c5952c78d241e03dae9a0f07c0d80fb
Nolij <xdmatthewbx@mixhosting.xyz>
2024-06-13 18:00:15 -0400

    fix Fabric 21 compat

--- modern/src/main/java/dev/nolij/zume/mixin/modern/CameraMixin.java
+++ modern/src/main/java/dev/nolij/zume/mixin/modern/CameraMixin.java
@@ -2,16 +2,26 @@ package dev.nolij.zume.mixin.modern;
 
 import dev.nolij.zume.impl.Zume;
 import net.minecraft.client.Camera;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(value = Camera.class, priority = 1500)
 public abstract class CameraMixin {
 	
-	@ModifyArg(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(D)D"))
+	@Group(name = "zume$thirdPersonCameraHook", min = 1, max = 1)
+	@ModifyArg(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(D)D"), require = 0)
 	public double zume$update$clipToSpace(double original) {
         return Zume.thirdPersonCameraHook(original);
+	}
+	
+	@Dynamic
+	@Group(name = "zume$thirdPersonCameraHook", min = 1, max = 1)
+	@ModifyArg(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_4184;method_19318(F)F", remap = false), require = 0)
+	public float zume$update$clipToSpace(float original) {
+        return (float) Zume.thirdPersonCameraHook(original);
 	}
 	
 }
--- modern/src/main/java/dev/nolij/zume/mixin/modern/GameRendererMixin.java
+++ modern/src/main/java/dev/nolij/zume/mixin/modern/GameRendererMixin.java
@@ -3,6 +3,7 @@ package dev.nolij.zume.mixin.modern;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.nolij.zume.impl.Zume;
 import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 	
-	@Inject(method = "render", at = @At("HEAD"))
+	@Dynamic
+	@Inject(method = {"render", "method_3192(Lnet/minecraft/class_9779;Z)V"}, at = @At("HEAD"))
 	public void zume$render$HEAD(CallbackInfo ci) {
 		Zume.renderHook();
 	}
