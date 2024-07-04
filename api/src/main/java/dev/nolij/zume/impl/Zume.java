f2a221179133e2b43d950c46850967a83b61437c
Nolij <xdmatthewbx@mixhosting.xyz>
2024-07-04 08:35:01 -0400

    misc clean-up

--- api/src/main/java/dev/nolij/zume/impl/Zume.java
+++ api/src/main/java/dev/nolij/zume/impl/Zume.java
@@ -6,7 +6,6 @@ import dev.nolij.zume.api.util.v1.EasingHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
